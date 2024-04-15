e730e5320c9972962894698da948cd368f5fae10
bossvirus03 <nguyenhuuloi17032004@gmail.com>
2024-04-15 13:13:33 +0700

    update ontime

--- src/modules/ontime.ts
+++ src/modules/ontime.ts
@@ -1,25 +1,21 @@
 import Ifca from "src/types/type.api";
 import * as cron from "node-cron";
-
-/*
-38 16 * * *
-    Trong đó:
-        38 là phần phút (38 phút)
-        16 là phần giờ (16 giờ)
-
-see https://www.npmjs.com/package/cron
-*/
+import { Thread } from "src/db/models/threadModel";
+import { UserInThread } from "src/db/models/userInThreadModel";
+import { User } from "src/db/models/userModel";
 class OnTime {
-  scheduleTask(api: Ifca) {
-    api.listenMqtt(async (err, event) => {
-      cron.schedule("38 16 * * *", () => {
-        console.log("Chạy công việc vào lúc 16:38 giờ hàng ngày");
-        api.sendMessage(
-          "Chạy công việc vào lúc 16:38 giờ hàng ngày",
-          event.threadID
-        );
+  async scheduleTask(api: Ifca) {
+    const threadData = await Thread.findAll();
+    const userData = await User.findAll();
+    const userInThreadData = await UserInThread.findAll();
+
+    // Automatically send a message at 05:00
+    cron.schedule("00 05 * * *", async () => {
+      threadData.map((item) => {
+        api.sendMessage("[Automation] Buổi sáng vui vẻ", item.dataValues.tid);
       });
     });
   }
 }
+
 export default OnTime;
