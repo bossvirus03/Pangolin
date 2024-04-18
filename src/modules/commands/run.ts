34c50a0068c9773f15878ce4c3280e7d8cdf7d21
NDK <80080316+dev-ndk@users.noreply.github.com>
2024-04-18 15:21:25 +0700

    Update the run command to add try catch and add some other features
    Update the run command to add try catch and add some other features

--- src/modules/commands/run.ts
+++ src/modules/commands/run.ts
@@ -4,23 +4,77 @@ export default class RunCommand {
   static config = {
     name: "run", //tên lệnh của bạn
     version: "1.0.0",
-    author: "Lợi",
+    author: "Lợi NDK-[FIX]",
     createdAt: "",
-    description: "Cách dùng: [prefix]run [your script]",
     permission: 2,
+    description: {
+      vi: "Test code nhanh",
+      en: "Test code quickly",
+    },
+    guide: {
+      vi: "[prefix]run <đoạn code cần test>",
+      en: "[prefix]run <code to test>",
+    },
+  };
+  static message = {
+    vi: {
+      error: "Đã xảy ra lỗi không xác định.",
+    },
+    en: {
+      error: "An unknown error has occurred.",
+    },
   };
 
-  constructor(private client) {}
-  async run({ api, event, client, args, UserData, ThreadData }: IPangolinRun) {
-    // const script = (event.body as string).split(args[0])[1].trim();
-    const script = `(api, event, client, args, UserData, ThreadData) => {
-      ${(event.body as string).split(args[0])[1].trim()}
-  }`;
-    try {
-      const scriptFunction = eval(script);
-      scriptFunction(api, event, client, args, UserData, ThreadData);
-    } catch (error) {
-      api.sendMessage("Lỗi khi thực thi mã: " + error, event.threadID);
+  constructor() {}
+  async run({
+    api,
+    event,
+    client,
+    args,
+    UserData,
+    ThreadData,
+    getLang,
+  }: IPangolinRun) {
+    function output(msg) {
+      if (
+        typeof msg == "number" ||
+        typeof msg == "boolean" ||
+        typeof msg == "function"
+      )
+        msg = msg.toString();
+      else if (msg instanceof Map) {
+        let text = `Map(${msg.size}) `;
+        text += JSON.stringify(mapToObj(msg), null, 2);
+        msg = text;
+      } else if (typeof msg == "object") msg = JSON.stringify(msg, null, 2);
+      else if (typeof msg == "undefined") msg = "undefined";
+
+      api.sendMessage(msg, event.threadID, () => {}, event.messageID);
     }
+
+    function out(msg) {
+      output(msg);
+    }
+
+    function mapToObj(map) {
+      const obj = {};
+      map.forEach(function (v, k) {
+        obj[k] = v;
+      });
+      return obj;
+    }
+
+    const run = `(async () => {
+			try {
+				${(event.body as string).split(args[0])[1].trim()}
+			}
+			catch(error) {
+        let message = await getLang("error");
+        if (error instanceof Error) message = error.message;
+				console.log(error);
+        api.sendMessage(message, event.threadID, () => {}, event.messageID);
+			}
+		})()`;
+    eval(run);
   }
 }
